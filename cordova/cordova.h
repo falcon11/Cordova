2dc8e75859e01b70d1228b252402024c76c2bd96
ashoka <savebtc11@gmail.com>
2019-05-05 15:46:36 +0800

    Initial Commit

---
+++ cordova/cordova.h
@@ -0,0 +1,19 @@
+//
+//  cordova.h
+//  cordova
+//
+//  Created by Ashoka on 2019/5/5.
+//  Copyright © 2019 Ashoka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cordova.
+FOUNDATION_EXPORT double cordovaVersionNumber;
+
+//! Project version string for cordova.
+FOUNDATION_EXPORT const unsigned char cordovaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cordova/PublicHeader.h>
+
+
